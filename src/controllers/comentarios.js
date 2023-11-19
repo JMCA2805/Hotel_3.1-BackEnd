39e47c51582ce856751c49d1e552f3f8d99f085b
JMCA2805 <josemauricio2805@gmail.com>
2023-11-19 01:27:17 -0400

    Fix controllers/comentarios.js

--- src/controllers/comentarios.js
+++ src/controllers/comentarios.js
@@ -9,12 +9,18 @@ const agregarComentario = async (req, res) => {
       comentario,
     };
 
+    if(!comentario){
+      res.status(500).send({ Error: "Porfavor ingrese un comentario" });
+      return
+    }
     const nuevoComentario = new Comentarios(cmntr);
 
     nuevoComentario.save();
 
-    res.status(200).send("¡Reseña Enviada con Exito!");
+    res.status(200).send({message:"¡Reseña Enviada con Exito!"});
   } catch (error) {
+    console.log(error)
+
     res.status(500).send({ Error: "Error al eviar la reseña" });
   }
 };
